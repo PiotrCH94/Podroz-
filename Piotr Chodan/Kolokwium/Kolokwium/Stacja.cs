e1e360bf62c11abe066216eaf63109e1a4b0d9cc
PiotrCH94 <piotrchodan94.pc@gmail.com>
2017-01-16 13:21:38 +0100

    Add files via upload

---
+++ Piotr Chodan/Kolokwium/Kolokwium/Stacja.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium
+{
+    class Stacja : ICloneable
+    {
+        private string nazwaStacji;
+        private bool oplataKlimatyczna; 
+        {
+            oplataKlimatyczna = true;
+            return MemberwiseClone();
+        }    
+        public Stacja (string nazwaStacji, bool oplataKlimatyczna)
+{
+    this.nazwaStacji = nazwaStacji;
+    this.oplataklimatyczna = oplataKlimatyczna;
+}
+        public JakaOplata()
+{
+    return oplataKlimatyczna;
+}
+    }
+}
